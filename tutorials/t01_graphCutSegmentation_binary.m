36f24c547d032d5b1ca59cc3977cb9138576d2cc
Martin Rajchl <m.rajchl@imperial.ac.uk>
2015-03-26 18:51:57 +0000

    ADD: First graph cut tutorials added

---
+++ tutorials/t01_graphCutSegmentation_binary.m
@@ -0,0 +1,74 @@
+%% Tutorial 01: Binary Graph cut segmentation
+%  Martin Rajchl, Imperial College London, 2015
+  
+clear all; close all;
+
+% 1. Load an 8-bit greyscale image:
+img = imread('../data/cameraman.tif');
+
+% 2. Normalize the image intensity to [0,1]:
+img = single(img);
+img_n = (img - min(img(:))) / (max(img(:)) - min(img(:)));
+
+% 3. Create two cost functions to model foreground (fg) and background (bg):
+
+% The costs are defined as L1 distances of the image to two intensity 
+% values we consider descriptive of fg and bg, respectively.
+val_fg = 0.75; val_bg = 0.25;
+
+% compute the L1 cost terms
+cost_fg = abs(img_n - val_fg);
+cost_bg = abs(img_n - val_bg);
+
+% visualize them
+figure(); 
+subplot(1,2,1); imshow(cost_fg,[]); title('cost_{fg}');
+subplot(1,2,2); imshow(cost_bg,[]); title('cost_{bg}');
+
+% 4. Construct an s-t graph:
+[sx, sy] = size(cost_fg);
+
+% allocate s and t links
+Cs = zeros(sx,sy);
+Ct = zeros(sx,sy);
+
+% allocate alpha(x), the regularization weight at each node x
+alpha = zeros(sx,sy);
+
+% 5. Assign capacities to the graph:
+% Assign the costs from 3. as capacities for the s-t links as data
+% consistency terms
+Cs = cost_bg;
+Ct = cost_fg;
+
+% Assign a regularization weight (equivalent to pairwise terms) for each
+% node x. Here we employ a constant regularization weight alpha. The higher
+% alpha is, the more smoothness penalty is assigned.
+alpha = 0.25.*ones(sx,sy);
+        
+% 6. Set up the parameters for the max flow optimizer:
+% [1] graph dimension 1
+% [2] graph dimension 2
+% [3] number of maximum iterations for the optimizer (default 200)
+% [4] an error bound at which we consider the solver converged (default
+%     1e-5)
+% [5] c parameter of the multiplier (default 0.2)
+% [6] step size for the gradient descent step when calulating the spatial
+%     flows p(x) (default 0.16)
+pars = [sx; sy; 200; 1e-5; 0.2; 0.16];
+    
+% 7. Call the binary max flow optimizer with Cs, Ct, alpha and pars to obtain
+% the continuous labelling function u, the convergence over iterations
+% (conv), the number of iterations (numIt) and the run time (time);
+[u, conv, numIt, time] = asetsBinaryMF2D(Cs, Ct, alpha, pars);
+
+% 8. Threshold the continuous labelling function u to obtain a discrete
+% segmentation result
+ut = u > 0.5;
+
+% 9. Visualize the orignial image and the segmentation 
+figure(); 
+subplot(1,3,1); imshow(img,[]); title('Original image');
+subplot(1,3,2); imshow(u,[]); title('Segmentation: u_{continuous}');
+subplot(1,3,3); imshow(ut,[]); title('Segmentation u_{discrete}');
+
---
+++ tutorials/t02_graphCutSegmentation_potts.m
@@ -0,0 +1,91 @@
+%% Tutorial 02: Multi-region graph cut color segmentation with the Potts model
+%  Martin Rajchl, Imperial College London, 2015
+  
+clear all; close all;
+
+% 1. Load a color image and cast to single:
+img = imread('../data/berkeley_color_124084.jpg');
+imgs = single(img);
+
+% 2. Create N = 4 cost functions to model each of the regions:
+numberOfLabels = 4;
+[sx, sy, rgb] = size(img);
+
+% The costs are defined as L1 distances of the rgb image to N color models:
+model(1,:) = [225,0,0]; % red
+model(2,:) = [0,150,50]; % green
+model(3,:) = [255,225,0]; % yellow
+model(4,:) = [0,0,0]; % black
+
+% compute the L1 cost term for each of the N = 4 regions:
+cost = zeros(sx, sy, numberOfLabels,class(imgs));
+for i=1:numberOfLabels
+    cost(:,:,i) = (abs(imgs(:,:,1) - model(i,1)) + abs(imgs(:,:,2) - model(i,2)) + abs(imgs(:,:,3) - model(i,3)));
+end
+
+% visualize them
+figure(); 
+subplot(2,3,1); imshow(img,[]); title('image');
+subplot(2,3,2); imshow(cost(:,:,1),[]); title('cost_{red}');
+subplot(2,3,3); imshow(cost(:,:,2),[]); title('cost_{green}');
+subplot(2,3,5); imshow(cost(:,:,3),[]); title('cost_{yellow}');
+subplot(2,3,6); imshow(cost(:,:,4),[]); title('cost_{black}');
+
+% 4. Construct the multi-label graph:
+% allocate the sink links Ct(x)
+Ct = zeros(sx,sy, numberOfLabels,class(imgs));
+
+% allocate alpha(x), the regularization weight at each node x
+alpha = zeros(sx,sy, numberOfLabels,class(imgs));
+
+% 5. Assign capacities to the graph:
+% Since this is a multi-label graph, where the source flows ps(x) are unconstrained,
+% we define our sink capacities Ct(x,l) for each label l:
+
+for i=1:numberOfLabels
+    Ct(:,:,i) = cost(:,:,i);
+end
+
+% Assign a regularization weight (equivalent to pairwise terms) for each
+% node x. Here we employ a constant regularization weight alpha(x,l). 
+% Note, that the original Potts model does not have alpha indexed by label.
+% This implementation is more flexible and represents a special case of 
+% the hierarchical max flow, the horizontal model. However, in this case 
+% we employ the orignial Potts model to multi-region segmentation 
+% with constant regularization. Further, the regularization weight needs to
+% be in a similar order of magnitude as Ct(x,l).
+alpha = 0.025*max(cost(:)).*ones(sx,sy,numberOfLabels,class(imgs));
+        
+% 6. Set up the parameters for the max flow optimizer:
+% [1] graph dimension 1
+% [2] graph dimension 2
+% [3] number of labels
+% [4] number of maximum iterations for the optimizer (default 300)
+% [5] an error bound at which we consider the solver converged (default
+%     1e-5)
+% [6] c parameter of the multiplier (default 0.2)
+% [7] step size for the gradient descent step when calulating the spatial
+%     flows p(x) (default 0.16)
+pars = [sx; sy; numberOfLabels; 200; 1e-5; 0.2; 0.16];
+    
+% 7. Call the Potts model max flow optimizer Ct(x,l), alpha(x) and pars to obtain
+% the continuous labelling function u(x,l), the convergence over iterations
+% (conv), the number of iterations (numIt) and the run time (time);
+[u, conv, numIt, time] = asetsPotts2D(Ct, alpha, pars);
+
+% 8. To discretize the continuous labelling function u(x,l) we employ a
+% majorty vote over l to obtain a final segmentation:
+[tmp, idx] = max(u, [], 3);
+
+% 9. Visualize the orignial image and the segmentation with the original
+% color models:
+seg = zeros(sx, sy, rgb, 'uint8');
+for x=1:sx
+    for y=1:sy
+        seg(x,y,:) = model(idx(x,y),:);
+    end
+end
+figure(); 
+subplot(1,2,1); imshow(img,[]); title('Original image');
+subplot(1,2,2); imshow(seg,[]); title('Segmentation u_{discrete}');
+
