ce8a6869dda3ef64a9bea3580ed227c41f2a59ea
Martin Rajchl <m.rajchl@imperial.ac.uk>
2016-02-17 16:16:02 +0000

    FIX: typo, missing includes, figure titles

--- tests/runTestHMFLinearModel.m
+++ tests/runTestHMFLinearModel.m
@@ -1,8 +1,12 @@
 function [] = runTestHMFLinearModel()
 
-%   John Baxyer, Robarts Research Institute, London, Ontario, 2015
+%   John Baxter, Robarts Research Institute, London, Ontario, 2015
 %   Testing scripts for hierarchical max flow regularization
 %   Test script comprises of a linear (Ishikawa) image reconstruction model
+
+% include max-flow solver
+addpath(['..', filesep, 'maxflow']);
+addpath(['..', filesep, 'lib']);
 
 alpha = 0.15;
 noise = 0.15;
@@ -46,13 +50,13 @@ end
 
 figure(2)
     subplot(4,1,1);
-        imshow(image(:,:,1),[0,1]);
+        imshow(image(:,:,1),[0,1]); title('Noisy image');
     subplot(4,1,2);
-        imshow(recon(:,:,1),[0,1]);
+        imshow(recon(:,:,1),[0,1]); title('Reconstructed image');
     subplot(4,1,3);
-        imshow(abs(recon(:,:,1)-imageDenoised(:,:,1)),[0 1]);
+        imshow(abs(recon(:,:,1)-imageDenoised(:,:,1)),[0 1]); 
     subplot(4,1,4);
-        imshow(abs(recon(:,:,1)-image(:,:,1)),[0 1]);
+        imshow(abs(recon(:,:,1)-image(:,:,1)),[0 1]); 
 
 if showVis 
     figure(1); clf;
